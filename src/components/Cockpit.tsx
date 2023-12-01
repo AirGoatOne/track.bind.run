2ccca6ed758398d1e722714f9aa3d6b6bfc454b3
AirGoatOne <64951391+AirGoatOne@users.noreply.github.com>
2023-12-01 18:25:54 +0100

    Make Cockpit responsive

--- src/components/Cockpit.tsx
+++ src/components/Cockpit.tsx
@@ -1,3 +1,4 @@
+import { IoChevronDown, IoChevronUp } from "react-icons/io5"
 import { useAppSelector } from "../app/hooks"
 import Controls from "../features/controls/Controls"
 import { selectCurrentTrackpoint } from "../features/controls/controlsSlice"
@@ -7,15 +8,28 @@ import Altitude from "./Altitude"
 import "./Cockpit.css"
 import Direction from "./Direction"
 import Elevation from "./Elevation"
+import { useState } from "react"
+import clsx from "clsx"
 
 export default function Cockpit() {
   const name = useAppSelector(selectGpxName)
   const currentTrackpoint = useAppSelector(selectCurrentTrackpoint)
+  const [expandControls, setExpandControls] = useState(false)
+  function toggleExpand() {
+    setExpandControls(!expandControls)
+  }
   return (
     <div className="Cockpit" role="table">
-      <div role="row">
+      <div
+        role="row"
+        className={clsx("Cockpit-controls", { open: expandControls })}
+      >
         <div role="cell" className="track-name">
           <h3>{name}</h3>
+          <div className="open-close" onClick={() => toggleExpand()}>
+            {!expandControls && <IoChevronUp />}
+            {expandControls && <IoChevronDown />}
+          </div>
         </div>
         <div role="cell">
           <Controls />
